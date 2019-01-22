13bf6e6c143df1d9abf184d4137d9fb1cf16e97c
Tsvetelina Stoyanova <cvetelinast.96@abv.bg>
2019-01-23 01:47:04 +0200

    The test script is create_db.sql, the original one is schema.sql

---
+++ server/database/config/sql/schema.sql
@@ -0,0 +1,35 @@
+create database if not exists `WEB_APP_STORE`;
+
+drop table if exists `USERS`;
+
+drop table if exists `APPLICATIONS`;
+
+drop table if exists `REVIEWS`;
+
+create table if not exists `USERS` (
+  `USERNAME` varchar(32) not null,
+  `EMAIL` varchar(32) not null,
+  `PASSWORD` varchar(32) not null,
+  primary key `USERNAME` (`USERNAME`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+create table if not exists `APPLICATIONS` (
+  `NAME` varchar(32) not null,
+  `DESCRIPTION` varchar(255),
+  `LOGO` longblob,
+  `SOURCE` longblob not null,
+  `ID` varchar(32) not null,
+  primary key `ID` (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+create table if not exists `REVIEWS` (
+  `SCORE` int not null,
+  `COMMENT` varchar(255),
+  `ID` varchar(32) not null,
+  `REVIEWER_NAME` varchar(32) not null,
+  `APPLICATION_ID` varchar(32) not null,
+  primary key `ID` (`ID`),
+  foreign key (`APPLICATION_ID`) references `APPLICATIONS`(`ID`) ON DELETE CASCADE
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+insert into `USERS` values ("admin", "admin@abv.bg", "21232f297a57a5a743894a0e4a801fc3")
